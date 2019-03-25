384807b330ab050354116b28a024a84374df500b
loribao <lorival.sanjinez@hotmail.com>
2019-03-25 01:22:34 -0300

    Atualizando sendo o video

--- code/snippets/simpleCrawler.js
+++ code/snippets/simpleCrawler.js
@@ -5,7 +5,7 @@ function crawlear(){
         var teste = /^(http[a-z]{0,1}:\/\/.+pdf)$/g
         for (var i = 0; i < arrTag.length; i++) {
             if(teste.test(arrTag[i].href)){
-            ret[i] = arrTag[i].href;
+            ret.push(arrTag[i].href);
             }
         }  
         return ret;
