6d1b51d6ac5cf2e5238c3dd4af34ec6c68f54b75
loribao <lorival.sanjinez@hotmail.com>
2019-03-26 12:20:00 -0300

    ajustando o conteudo

---
+++ aula 0 - introducao (codigos)/snippets/simpleCrawler.js
@@ -0,0 +1,13 @@
+
+function crawlear(){
+        var ret = [];
+        var arrTag =  window.document.getElementsByTagName('a');
+        var teste = /^(http[a-z]{0,1}:\/\/.+pdf)$/g
+        for (var i = 0; i < arrTag.length; i++) {
+            if(teste.test(arrTag[i].href)){
+            ret.push(arrTag[i].href);
+            }
+        }  
+        return ret;
+}
+copy(crawlear());
